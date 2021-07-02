e9c0f96feaece5b60984967e606c9a69b41538c7
paradajz <2544094+paradajz@users.noreply.github.com>
2021-07-02 22:38:43 +0200

    disable scrolling when changing section

--- js/main.js
+++ js/main.js
@@ -192,9 +192,7 @@
        var target = $(this.hash);
        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
        if (target.length) {
-        $('html, body').animate({
-           scrollTop: target.offset().top
-        }, 1000);
+        $('html, body').scrollTop(target.offset().top);
         return false;
        }
       }
