eb4bd95a3d02a4aaa8e47274b045c7f53c5dfd7d
web_bot <2544094+web_bot@users.noreply.github.com>
2022-01-17 10:28:30 +0000

    update

---
+++ js/blog/blog.js
@@ -0,0 +1,2 @@
+!function(e){"use strict";e(".nav-button").on("click",(function(){e("body").toggleClass("nav-open")})),e(".related-post-carousel").owlCarousel({dots:!1,nav:!1,margin:30,autoplay:!0,responsive:{0:{items:1},600:{items:2},1e3:{items:3}}}),(new WOW).init()}(jQuery);
+//# sourceMappingURL=blog.js.map
